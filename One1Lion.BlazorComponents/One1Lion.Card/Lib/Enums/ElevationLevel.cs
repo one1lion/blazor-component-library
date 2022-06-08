f1ba197f981e8f963630167a3d71a82ca49d0f76
one1lion <i_am_listening@hotmail.com>
2022-06-08 12:40:23 -0600

    Added some of the Card Styles and started adding a preview sample page

---
+++ One1Lion.BlazorComponents/One1Lion.Card/Lib/Enums/ElevationLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace One1Lion.Card.Lib.Enums;
+
+public enum ElevationLevel
+{
+    Flat,
+    Low,
+    Medium,
+    High
+}
---
+++ One1Lion.BlazorComponents/One1Lion.Samples/Helpers/ColorHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Linq;
+
+namespace One1Lion.Samples.Helpers;
+
+public static class ColorHelpers
+{
+    public static Color FromHex(string hex, float alpha = 1.0f)
+    {
+        hex = hex.Replace("#", "");
+        var useAlpha = (int)(100 * alpha);
+
+        // TODO: finish the logic to cast from hex to decimal for each color
+        switch(hex.Length)
+        {
+            case 3:
+                var colArr = hex.Split();
+                var r = $"{colArr[0]}{colArr[0]}";
+                return Color.FromArgb(useAlpha, Color.Black);
+            case 6:
+
+                return Color.FromArgb(useAlpha, Color.Black);
+            default:
+                return Color.FromArgb(useAlpha, Color.Black);
+        }
+    }  
+}
